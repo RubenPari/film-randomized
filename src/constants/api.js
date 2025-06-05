08930e792aeae798b3cbbe3cab8ca0cadd575192
RubenPari <rubenpari.developer@gmail.com>
2025-06-05 15:16:30 +0200

    laod constants from env variables

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
-export const API_KEY = 'd8d1e982f6cd7a2fe927be7ba4c903f1';
-export const BASE_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+export const API_KEY = import.meta.env.VITE_API_KEY;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
 
 export const API_ENDPOINTS = {
     movieGenres: `${BASE_URL}/genre/movie/list`,
